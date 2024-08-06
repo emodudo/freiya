c21563a5fa077219f4ad422a02fb926a00c54092
emodudo <eduradombc@gmail.com>
2024-08-06 10:08:15 -0300

    new animations and images

--- src/app/sobre/sobre.component.ts
+++ src/app/sobre/sobre.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
-  styleUrl: './sobre.component.css'
+  styleUrls: ['./sobre.component.css']
 })
-export class SobreComponent {
+export class SobreComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.checkScroll();
+  }
+
+  checkScroll(): void {
+    const elements = document.querySelectorAll('.animated-text');
+    elements.forEach(element => {
+      const position = element.getBoundingClientRect();
+      if (position.top >= 0 && position.bottom <= window.innerHeight) {
+        element.classList.add('animated');
+        element.classList.remove('hidden');
+      }
+    });
+  }
 }
