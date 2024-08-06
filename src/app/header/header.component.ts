b8e1119cdb9c0f878a3759e3aa5e26614a055ad9
emodudo <eduradombc@gmail.com>
2024-08-05 22:52:57 -0300

    novas animações e troca de imagem.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
 
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.onScroll);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.onScroll);
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const navbar = document.querySelector('.nav-freiya') as HTMLElement;
+    if (window.scrollY > 50) { // você pode ajustar o valor de 50 conforme a necessidade
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  }
 }
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrls: ['./hero.component.css']
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit, AfterViewInit, OnDestroy {
+  private images: HTMLElement[] = [];
+  private currentIndex: number = 0;
+  private interval: any;
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.images = Array.from(document.querySelectorAll('.custom-video'));
+    this.startImageRotation();
+  }
+
+  startImageRotation(): void {
+    if (this.images.length === 0) return;
+
+    this.images[this.currentIndex].classList.add('active');
+    this.interval = setInterval(() => {
+      this.images[this.currentIndex].classList.remove('active');
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      this.images[this.currentIndex].classList.add('active');
+    }, 4000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 }
